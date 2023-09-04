6a69d559b2837e442c12e55e2ccfae07b74a65d2
Hussein Ahmad <husseinahmad@microsoft.com>
2023-09-04 19:16:40 +0100

    Fav

--- src/my-favorite-items/index.ts
+++ src/my-favorite-items/index.ts
@@ -1,114 +1,287 @@
-import { ArcGISIdentityManager } from '@esri/arcgis-rest-request';
-import {
-    shareItemWithGroup,
-    unshareItemWithGroup,
-    isItemSharedWithGroup,
-    ISharingResponse,
-    IGroupSharingOptions,
-    searchGroupContent,
-} from '@esri/arcgis-rest-portal';
-import { IItem } from '@esri/arcgis-rest-types';
-
-import { IServerInfo } from '@esri/arcgis-rest-request';
-
+import axios from 'axios';
 import { defaultOptions } from '..';
 
-// let favGroupId = '';
-// let session:UserSession = null;
 
-// export const setMyFavoriteGroup = ({
-//     groupId,
-//     userSession
-// }: {
-//     groupId: string;
-//     userSession: UserSession;
-// }):void => {
-
-//     if(!groupId){
-//         console.error('My Favorite group ID is required');
-//         return;
-//     }
-
-//     if(!userSession){
-//         console.error('user session is required');
-//         return;
-//     }
-
-//     favGroupId = groupId;
-//     session = userSession;
-// };
-
-export const getMyFavItems = async (): Promise<IItem[]> => {
-    const { myFavGroupId } = defaultOptions;
-
-    const serverInfo: IServerInfo = {
-        hasPortal: true,
-        hasServer: false,
-        server: `${defaultOptions.ArcGISOnlineHost} "/sharing/rest`,
-        owningSystemUrl: null,
-    };
-
-    const identidyManager = ArcGISIdentityManager.fromCredential(
-        defaultOptions.credential,
-        serverInfo
-    );
-
-    if (!identidyManager || !myFavGroupId) {
-        return [];
-    }
-
-    try {
-        const response = await searchGroupContent({
-            groupId: myFavGroupId,
-            q: '',
-            num: 1000,
-            authentication: identidyManager,
-        });
-
-        return response.results;
-    } catch (err) {
-        throw err;
-    }
+interface AgolItem {
+    id: string;
+    title: string;
+    type: string;
+    owner?: string;
+    typeKeywords?: string[];
+    description?: string;
+    snippet?: string;
+    documentation?: string;
+    extent?: number[][];
+    categories?: string[];
+    culture?: string;
+    properties?: any;
+    url?: string;
+    tags?: string[];
+    thumbnail?: string;
+    [key: string]: any;
 };
 
-export const toggleShareWithMyFavGroup = async (
-    itemId: string
-): Promise<ISharingResponse> => {
-    const { myFavGroupId } = defaultOptions;
+interface ShareOperationResponse {
+    itemId: string;
+};
 
-    const serverInfo: IServerInfo = {
-        hasPortal: true,
-        hasServer: false,
-        server: `${defaultOptions.ArcGISOnlineHost} "/sharing/rest`,
-        owningSystemUrl: null,
-    };
+let myFavItemIds:string[] = [];
 
-    const identidyManager = ArcGISIdentityManager.fromCredential(
-        defaultOptions.credential,
-        serverInfo
-    );
+const STORAGE_KEY_ADD_2_MY_FAV = 'itemToBeAddedToAgolFavGroup';
 
-    if (!identidyManager) {
-        throw {
-            error: 'need to sign in before toggle sharing item with my fav group',
+const setMyFavItemIds = (itemIds:string[])=>{
+    myFavItemIds = itemIds;
+}
+
+const getMyFavItems = (): Promise<AgolItem[]>=>{
+
+    const { myFavGroupId, token, ArcGISOnlineHost } = defaultOptions;
+
+    return new Promise(async(resolve, reject)=>{
+
+        if(!myFavGroupId || !token){
+            reject('skip query my fav items for anonymous user');
+        }
+
+        const requestUrl = `${ArcGISOnlineHost}/sharing/rest/search`;
+
+        const params = {
+            start: 1,
+            num: 1000,
+            q: `(group:${myFavGroupId})`,
+            f: 'json',
+            token
         };
+
+        axios.get(requestUrl, { params })
+        .then(res=>{
+            if(res && res.data && res.data.results){
+                resolve(res.data.results);
+            } else {
+                reject('')
+            }
+        }).catch(err=>{
+            reject(err);
+        });
+
+    });
+};
+
+const addToMyFavGroup = (itemId=''): Promise<ShareOperationResponse>=>{
+
+    const { myFavGroupId, token, ArcGISOnlineHost } = defaultOptions;
+
+    return new Promise(async(resolve, reject)=>{
+
+        if(!myFavGroupId || !token){
+            sessionStorage.setItem(STORAGE_KEY_ADD_2_MY_FAV, itemId);
+
+            // console.error('favorite group id and token are required to add fav item');
+            reject({
+                message: 'favorite group id and token are required to add fav item, please sign in first',
+                isSignInRequired: true
+            });
+
+        } else {
+
+            try {
+                const toggleShareAsMyFavRes = await toggleShareAsMyFav(itemId, true);
+                resolve(toggleShareAsMyFavRes);
+            } catch (err){
+                reject(err);
+            }
+
+        }
+    });
+};
+
+const removeFromMyFavGroup = (itemId=''): Promise<ShareOperationResponse>=>{
+
+    const { myFavGroupId, token } = defaultOptions;
+
+    return new Promise(async(resolve, reject)=>{
+            
+        if(!myFavGroupId || !token){
+
+            // console.error('favorite group id and token are required to add fav item');
+            reject({
+                message: 'favorite group id and token are required to remove fav item, please sign in first',
+                isSignInRequired: true
+            });
+
+        } else {
+
+            try {
+                const toggleShareAsMyFavRes = await toggleShareAsMyFav(itemId, false);
+                resolve(toggleShareAsMyFavRes);
+            } catch (err){
+                reject(err);
+            }
+        }
+    })
+};
+
+const addUnsavedItemsToMyFav = async(): Promise<string[]>=>{
+
+    const val = sessionStorage.getItem(STORAGE_KEY_ADD_2_MY_FAV);
+    sessionStorage.removeItem(STORAGE_KEY_ADD_2_MY_FAV);
+
+    if(!val){
+        return []
     }
+
+    const itemIds = val.split(',')
 
     try {
-        const options: IGroupSharingOptions = {
-            groupId: myFavGroupId,
-            id: itemId,
-            authentication: identidyManager,
-        };
 
-        const isSharedWithMyFavGroup = await isItemSharedWithGroup(options);
+        const requests = itemIds.map(itemId=>toggleShareAsMyFav(itemId, true))
 
-        const response: ISharingResponse = isSharedWithMyFavGroup
-            ? await unshareItemWithGroup(options)
-            : await shareItemWithGroup(options);
+        await Promise.all(requests)
 
-        return response;
-    } catch (err) {
-        throw err;
+        return itemIds
+    } catch(err){
+        console.log(err);
     }
+
+    return []
+};
+
+const toggleMyFavItemIds = (itemId:string)=>{
+
+    const idx = myFavItemIds.indexOf(itemId);
+    const newMyFavItemIds = [...myFavItemIds];
+
+    if(idx > -1){
+        newMyFavItemIds.splice(idx, 1);
+    } else {
+        newMyFavItemIds.push(itemId);
+    }
+
+    setMyFavItemIds(newMyFavItemIds);
+}
+
+const toggleShareAsMyFav = (itemId:string, isAddingToMyFav:boolean): Promise<ShareOperationResponse>=>{
+
+    const { myFavGroupId, token, ArcGISOnlineHost } = defaultOptions;
+
+    const operation = isAddingToMyFav ? 'share' : 'unshare';
+
+    const requestUrl = `${ArcGISOnlineHost}/sharing/rest/content/items/${itemId}/${operation}`;
+
+    const bodyFormData = new FormData();
+    bodyFormData.append('groups', myFavGroupId);
+    bodyFormData.append('items', itemId);
+    bodyFormData.append('token', token);
+    bodyFormData.append('f', 'json');
+
+    // console.log(bodyFormData);
+
+    return new Promise((resolve, reject) => {
+        axios.post(requestUrl, bodyFormData)
+        .then((response)=>{
+            // console.log(response);
+            resolve(response.data);
+        })
+        .catch(err => {
+            console.error(err);
+            reject(err);
+        });
+    });
+};
+
+export const getMyFavItemIds = async()=>{
+
+    let myFavItemIds: string[] = [];
+
+    const { myFavGroupId, token } = defaultOptions;
+
+    if(!myFavGroupId && !token){
+        return myFavItemIds;
+    }
+    
+    try {
+        const myFavItems = await getMyFavItems();
+
+        myFavItemIds = myFavItems.map(d=>{
+            return d.id;
+        });
+
+        // there could be item in the session storage that still needs to be share to the fav group,
+        // this is caused by attempting to add item to my fav group anomanonsly. 
+        // so we need to make sure this item is shared to fav and add the id to myFavItemIds
+        const idsForUnsavedItem = await addUnsavedItemsToMyFav();
+
+        if(idsForUnsavedItem){
+            
+            for(const id of idsForUnsavedItem){
+                if(myFavItemIds.indexOf(id) === -1){
+                    myFavItemIds.push(id);
+                }
+            }
+        }
+
+    } catch(err){
+        // console.log('my fav items not found');
+        console.log(err);
+    }
+
+    setMyFavItemIds(myFavItemIds);
+
+    return myFavItemIds;
+};
+
+export const toggleShareWithMyFavGroup = (itemId:string): Promise<string[]>=>{
+
+    const isInMyFavItems = myFavItemIds.indexOf(itemId) > -1 ? true : false;
+
+    return new Promise(async(resolve, reject)=>{
+
+        try {
+                
+            if(isInMyFavItems){
+                const removeFromMyFavGroupRes = await removeFromMyFavGroup(itemId);
+                // console.log('removeFromMyFavGroupRes', removeFromMyFavGroupRes)
+            } else {
+                const addToMyFavGroupRes = await addToMyFavGroup(itemId);
+                // console.log('addToMyFavGroupRes', addToMyFavGroupRes)
+            }
+    
+            toggleMyFavItemIds(itemId);
+
+            resolve(myFavItemIds);
+            
+        } catch(err){
+            // console.log(err);
+            reject(err);
+        }
+
+    });
+
+};
+
+export const batchAdd = (itemIds: string[]): Promise<string[]>=>{
+
+    return new Promise(async(resolve, reject)=>{
+
+        const { myFavGroupId, token } = defaultOptions;
+
+        if(!myFavGroupId || !token){
+            sessionStorage.setItem(STORAGE_KEY_ADD_2_MY_FAV, itemIds.join(','));
+            reject('favorite group id and token are required to remove fav item, please sign in first');
+            return
+        }
+
+        try {
+
+            for(let i = 0, len = itemIds.length; i < len; i++){
+                const itemId = itemIds[i]
+                const addToMyFavGroupRes = await addToMyFavGroup(itemId);
+                toggleMyFavItemIds(itemId);
+            }
+    
+            resolve(myFavItemIds);
+        } catch(err){
+            reject(err);
+        }
+    });
 };
